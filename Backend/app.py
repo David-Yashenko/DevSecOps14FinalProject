a7a7f51aaa8c591aa926dbe79b62ccb6ca7e812c
David-Yashenko <155112576+David-Yashenko@users.noreply.github.com>
2024-06-25 00:11:07 +0300

    change port to align with gunicorn

--- Backend/app.py
+++ Backend/app.py
@@ -13,4 +13,4 @@ api.add_resource(Check, '/check')
 api.add_resource(User, '/<usr>')
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=8000)
